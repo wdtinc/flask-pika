71644d5107428b52bbed176d159bd6863459c1f4
Karl Kirch <kkirch@wdtinc.com>
2014-03-11 11:16:04 -0500

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Pika',
-    version='0.3.1',
+    version='0.3.2',
     url='https://github.com/WeatherDecisionTechnologies/flask-pika',
     license='BSD',
     author='Weather Decision Technologies',
